241f276ca5b08f6d30d8b487ebdbc6b52fb67787
Apodemus Erectus <bnkho98@hotmail.com>
2021-09-04 20:18:48 +0200

    fix methods

--- services/AnimalService.lua
+++ services/AnimalService.lua
@@ -7,11 +7,23 @@ include "scenario/scripts/ui.lua"
 --- Service for modifiying animals.
 AnimalService = {}
 
---- Delete animal
+---- ADOPTION ----
+
+--- Get whether or not an animal can be put up for adoption
 --- @param animal animal
-function AnimalService.delete (animal)
-    deleteEntity(animal)
+--- @return bool
+function AnimalService.getAdoptOption (animal)
+    return animal:BFG_GET_ATTR_BOOLEAN("b_showAdopt")
 end
+
+--- Set whether or not an animal can be put up for adoption
+--- @param animal animal
+--- @param canBeAdopted bool
+function AnimalService.setAdoptOption (animal, canBeAdopted)
+    animal:BFG_SET_ATTR_BOOLEAN("b_showAdopt", canBeAdopted)
+end
+
+---- AGE ----
 
 --- Get animal age, returns true if adult, false if young
 --- @param animal animal
@@ -31,6 +43,88 @@ function AnimalService.getAgeString (animal)
     return "Young"
 end
 
+--- Set animal age
+--- @param animal animal
+--- @param age string
+function AnimalService.setAge (animal, age)
+    local species = animal:BFG_GET_ATTR_STRING("s_Species")
+    local gender = AnimalService.getGenderString(animal)
+    local super = AnimalService.getSuperString(animal)
+
+    animal:BFG_ENTITY_MORPH_TO_NEW_ENTITY(species .. "_" .. age .. "_" .. gender .. super, false, 0, false, 1)
+end
+
+---- BATHROOM ----
+
+--- Get animal bathroom stat
+--- @param animal animal
+--- @return float
+function AnimalService.getBathroom (animal)
+    return animal:BFG_GET_ATTR_FLOAT("bathroom")
+end
+
+--- Set animal bathroom stat
+--- @param animal animal
+--- @param exercise float
+function AnimalService.setBathroom (animal, bathroom)
+    setNeed(animal, "bathroom", bathroom)
+end
+
+---- CRATE ----
+
+--- Set animal crated status
+--- @param animal animal
+--- @param isCrated bool
+function AnimalService.setCrated (animal, isCrated)
+    if isCrated then
+        crateEntity(animal)
+    else
+        uncrateEntity(animal)
+    end
+end
+
+---- DELETE ----
+
+--- Delete animal
+--- @param animal animal
+function AnimalService.delete (animal)
+    deleteEntity(animal)
+end
+
+---- ENVIROMENT ----
+
+--- Get animal enviroment suitability
+--- @param animal animal
+--- @return float
+function AnimalService.getEnviroment (animal)
+    return animal:BFG_GET_ATTR_FLOAT("enviroment")
+end
+
+--- Set animal enviroment suitability
+--- @param animal animal
+--- @param enviroment float
+function AnimalService.setEnviroment (animal, enviroment)
+    setNeed(animal, "enviroment", enviroment)
+end
+
+---- EXERCISE ----
+
+--- Get animal exercise stat
+--- @param animal animal
+--- @return float
+function AnimalService.getExercise (animal)
+    return animal:BFG_GET_ATTR_FLOAT("exercise")
+end
+
+--- Set animal exercise stat
+--- @param animal animal
+--- @param exercise float
+function AnimalService.setExercise (animal, exercise)
+    setNeed(animal, "exercise", exercise)
+end
+
+---- GENDER ----
+
 --- Get animal gender, returns true if male, false if female
 --- @param animal animal
 --- @return bool
@@ -49,102 +143,6 @@ function AnimalService.getGenderString (animal)
     return "F"
 end
 
---- Get animal rampage status
---- @param animal animal
---- @return bool
-function AnimalService.getRampage (animal)
-    return animal:BFG_GET_ATTR_BOOLEAN("b_Rampage")
-end
-
---- Get animal rest status
---- @param animal animal
---- @return float
-function AnimalService.getRest (animal)
-    return animal:BFG_GET_ATTR_FLOAT("b_Rest")
-end
-
---- Get animal species
---- @param animal animal
---- @return string
-function AnimalService.getSpecies (animal)
-    return animal:BFG_GET_ATTR_STRING("s_Species")
-end
-
---- Get animal super status as sting
---- @param animal animal
---- @return string
-function AnimalService.getSuperString (animal)
-    local isSuper = animal:BFG_GET_ATTR_BOOLEAN("b_Super")
-    if isSuper then
-        return "_Super"
-    end
-    return ""
-end
-
---- Get animal thirst
---- @param animal animal
---- @return float
-function AnimalService.getThirst (animal)
-    return animal:BFG_GET_ATTR_FLOAT("b_Thirst")
-end
-
---- Set whether or not an animal can be put up for adoption
---- @param animal animal
---- @param canBeAdopted bool
-function AnimalService.setAdoptOption (animal, canBeAdopted)
-    animal:BFG_SET_ATTR_BOOLEAN("b_showAdopt", canBeAdopted)
-end
-
---- Set animal age
---- @param animal animal
---- @param age string
-function AnimalService.setAge (animal, age)
-    local species = animal:BFG_GET_ATTR_STRING("s_Species")
-    local gender = AnimalService.getGenderString(animal)
-    local super = AnimalService.getSuperString(animal)
-
-    animal:BFG_ENTITY_MORPH_TO_NEW_ENTITY(species .. "_" .. age .. "_" .. gender .. super, false, 0, false, 1)
-end
-
---- Set animal crated status
---- @param animal animal
---- @param isCrated bool
-function AnimalService.setCrated (animal, isCrated)
-    if isCrated then
-        crateEntity(animal)
-    else
-        uncrateEntity(animal)
-    end
-end
-
---- Set animal enviroment suitability
---- @param animal animal
---- @param enviroment float
-function AnimalService.setEnviroment (animal, enviroment)
-    setNeed(animal, "enviroment", enviroment)
-end
-
---- Set animal exercise stat
---- @param animal animal
---- @param exercise float
-function AnimalService.setExercise (animal, exercise)
-    setNeed(animal, "exercise", exercise)
-end
-
---- Set animal hunger
---- @param animal animal
---- @param hunger float
-function AnimalService.setHunger (animal, hunger)
-    setNeed(animal, "hunger", hunger)
-end
-
---- Set animal hygiene
---- @param animal animal
---- @param hygiene float
-function AnimalService.setHygiene (animal, hygiene)
-    setNeed(animal, "hygiene", hygiene)
-end
-
 --- Set animal gender
 --- @param animal animal
 --- @param gender string
@@ -154,6 +152,63 @@ function AnimalService.setGender (animal, gender)
     local super = AnimalService.getSuperString(animal)
 
     animal:BFG_ENTITY_MORPH_TO_NEW_ENTITY(species .. "_" .. age .. "_" .. gender .. super, false, 0, false, 1)
+end
+
+---- HEALTH ----
+
+--- Get animal health
+--- @param animal animal
+--- @return float
+function AnimalService.getHealth (animal)
+    return animal:BFG_GET_ATTR_FLOAT("health")
+end
+
+--- Set animal health
+--- @param animal animal
+--- @param exercise float
+function AnimalService.setExercise (animal, health)
+    setNeed(animal, "health", health)
+end
+
+---- HUNGER ----
+
+--- Get animal hunger
+--- @param animal animal
+--- @return float
+function AnimalService.getHunger (animal)
+    return animal:BFG_GET_ATTR_FLOAT("hunger")
+end
+
+--- Set animal hunger
+--- @param animal animal
+--- @param hunger float
+function AnimalService.setHunger (animal, hunger)
+    setNeed(animal, "hunger", hunger)
+end
+
+---- HYGIENE ----
+
+--- Get animal hygiene
+--- @param animal animal
+--- @return float
+function AnimalService.getHygiene (animal)
+    return animal:BFG_GET_ATTR_FLOAT("hygiene")
+end
+
+--- Set animal hygiene
+--- @param animal animal
+--- @param hygiene float
+function AnimalService.setHygiene (animal, hygiene)
+    setNeed(animal, "hygiene", hygiene)
+end
+
+---- PREGNANCY ----
+
+--- Get if animal is pregnant
+--- @param animal animal
+--- @return float
+function AnimalService.getPregnant (animal)
+    return animal:BFG_GET_ATTR_BOOLEAN("b_Pregnant")
 end
 
 --- Set animal pregnancy
@@ -174,11 +229,45 @@ function AnimalService.setPregnant (animal, isPregnant)
     end
 end
 
+---- PRIVACY ----
+
+--- Get animal privacy
+--- @param animal animal
+--- @return float
+function AnimalService.getPrivacy (animal)
+    return animal:BFG_GET_ATTR_FLOAT("privacy")
+end
+
 --- Set animal privacy
 --- @param animal animal
 --- @param privacy float
 function AnimalService.setPrivacy (animal, privacy)
     setNeed(animal, "privacy", privacy)
+end
+
+---- RAMPAGE ----
+
+--- Get animal rampage status
+--- @param animal animal
+--- @return bool
+function AnimalService.getRampage (animal)
+    return animal:BFG_GET_ATTR_BOOLEAN("b_Rampage")
+end
+
+--- Trigger a rampage
+--- @param animal animal
+function AnimalService.triggerRampage (animal)
+    local rampageManager = queryObject("BFGRampageMgr")
+    rampageManager:BFG_FORCE_RAMPAGE(animal)
+end
+
+---- RELEASE ----
+
+--- Get whether or not an animal can be released to the wild
+--- @param animal animal
+--- @return bool
+function AnimalService.getReleaseOption (animal)
+    return animal:BFG_GET_ATTR_BOOLEAN("b_showRelease")
 end
 
 --- Set whether or not an animal can be released to the wild
@@ -188,11 +277,29 @@ function AnimalService.setReleaseOption (animal, canBeReleased)
     animal:BFG_SET_ATTR_BOOLEAN("b_showRelease", canBeReleased)
 end
 
---- Set animal rest
+---- REST ----
+
+--- Get animal rest status
+--- @param animal animal
+--- @return float
+function AnimalService.getRest (animal)
+    return animal:BFG_GET_ATTR_FLOAT("rest")
+end
+
+--- Set animal rest status
 --- @param animal animal
 --- @param rest float
 function AnimalService.setRest (animal, rest)
     setNeed(animal, "rest", rest)
+end
+
+---- SOCIAL ----
+
+--- Get animal social stat
+--- @param animal animal
+--- @return float
+function AnimalService.getSocial (animal)
+    return animal:BFG_GET_ATTR_FLOAT("social")
 end
 
 --- Set animal social stat
@@ -202,11 +309,60 @@ function AnimalService.setSocial (animal, social)
     setNeed(animal, "social", social)
 end
 
---- Set animal stimulation stat
+---- SPECIES ----
+
+--- Get animal species
+--- @param animal animal
+--- @return string
+function AnimalService.getSpecies (animal)
+    return animal:BFG_GET_ATTR_STRING("s_Species")
+end
+
+--- Set animal species
+--- @param animal animal
+--- @param setSpecies string
+function AnimalService.setSpecies (animal, species)
+    local gender = AnimalService.getGenderString(animal)
+    local age = AnimalService.getAgeString(animal)
+    local super = AnimalService.getSuperString(animal)
+
+    animal:BFG_ENTITY_MORPH_TO_NEW_ENTITY(species .. "_" .. age .. "_" .. gender .. super, false, 0, false, 1)
+end
+
+---- STIMULATION ----
+
+--- Get animal stimulation
+--- @param animal animal
+--- @return float
+function AnimalService.getStimulation (animal)
+    return animal:BFG_GET_ATTR_FLOAT("stimulation")
+end
+
+--- Set animal stimulation
 --- @param animal animal
 --- @param stimulation float
 function AnimalService.setStimulation (animal, stimulation)
     setNeed(animal, "stimulation", stimulation)
+end
+
+---- SUPER ----
+
+--- Get animal super status as a boolean
+--- @param animal animal
+--- @return bool
+function AnimalService.getSuper (animal)
+    return animal:BFG_GET_ATTR_BOOLEAN("b_Super")
+end
+
+--- Get animal super status as sting
+--- @param animal animal
+--- @return string
+function AnimalService.getSuperString (animal)
+    local isSuper = animal:BFG_GET_ATTR_BOOLEAN("b_Super")
+    if isSuper then
+        return "_Super"
+    end
+    return ""
 end
 
 --- Set animal super status
@@ -224,15 +380,13 @@ function AnimalService.setSuper (animal, isSuper)
     end
 end
 
---- Set animal species
---- @param animal animal
---- @param setSpecies string
-function AnimalService.setSpecies (animal, species)
-    local gender = AnimalService.getGenderString(animal)
-    local age = AnimalService.getAgeString(animal)
-    local super = AnimalService.getSuperString(animal)
+---- THIRST ----
 
-    animal:BFG_ENTITY_MORPH_TO_NEW_ENTITY(species .. "_" .. age .. "_" .. gender .. super, false, 0, false, 1)
+--- Get animal thirst
+--- @param animal animal
+--- @return float
+function AnimalService.getThirst (animal)
+    return animal:BFG_GET_ATTR_FLOAT("thirst")
 end
 
 --- Set animal thirst
@@ -240,11 +394,4 @@ end
 --- @param thirst float
 function AnimalService.setThirst (animal, thirst)
     setNeed(animal, "thirst", thirst)
-end
-
---- Trigger a rampage
---- @param animal animal
-function AnimalService.triggerRampage (animal)
-    local rampageManager = queryObject("BFGRampageMgr")
-    rampageManager:BFG_FORCE_RAMPAGE(animal)
 end

0a130571a531dbaafc2eb0a5ec307aaba7e46df0
Apodemus Erectus <bnkho98@hotmail.com>
2021-10-04 14:31:54 +0200

    added big arluq class

---
+++ scripts/arluq.lua
@@ -0,0 +1,19 @@
+include "modules/ZtModArchive/ArluqTools/scripts/lua/logging.lua"
+include "modules/ZtModArchive/ArluqTools/scripts/lua/try.lua"
+
+include "modules/ZtModArchive/ArluqTools/scripts/services/AnimalService.lua"
+include "modules/ZtModArchive/ArluqTools/scripts/services/CollectionService.lua"
+include "modules/ZtModArchive/ArluqTools/scripts/services/FenceService.lua"
+include "modules/ZtModArchive/ArluqTools/scripts/services/GuestService.lua"
+
+--- Big Daddy Arluq class
+aq = {}
+
+aq.log = log
+aq.try = try
+aq.tryCatch = tryCatch
+
+aq.collection = CollectionService
+aq.animal = AnimalService
+aq.fence = FenceService
+aq.guest = GuestService
